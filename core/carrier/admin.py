301a0184b6f49785c5d3cc5a8d94283aeb897b2a
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-10-01 20:02:23 -0300

    feat: Registrar modelos de Delivery, Mark, Order, Payment e Vehicle no admin [Refs #11]

--- core/carrier/admin.py
+++ core/carrier/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from carrier.models import Delivery, Mark, Order, Payment, Vehicle
 
-# Register your models here.
+admin.site.register(Delivery)
+admin.site.register(Mark)
+admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(Vehicle)
