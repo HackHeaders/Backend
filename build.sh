a9eae5f777dd113c37ea426f6ccc0e71630b17b6
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-12-04 01:19:11 -0300

    FIX: remove execução do worker do Celery em segundo plano no script de build

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ pip install --upgrade pip
 # Instala as dependências
 pip install -r requirements.txt
 
-celery -A config.celery worker --loglevel=info --logfile=/var/log/celery/celery.log &
+celery -A config.celery worker --loglevel=info --logfile=/var/log/celery/celery.log
