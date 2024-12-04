9dc324a24842d6e807b4cef33bc160fb21815d41
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-12-03 22:18:48 -0300

    FIX: corrige comando para iniciar o worker do Celery no script de build

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ python manage.py collectstatic --no-input
 # Aplica as migrações
 python manage.py migrate
 
-python manage.py run celery -A config.celery worker --loglevel=info
+python manage.py celery -A config.celery worker --loglevel=info
