a7d423e1a63f7e2d7999f22ac1e62719514f01e3
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-12-10 19:52:45 -0300

    FEAT: traduzir mensagens de resposta para português e adicionar verificação de status para motorista e veículo ocupados

--- core/carrier/utils/assignVD.py
+++ core/carrier/utils/assignVD.py
@@ -16,28 +16,37 @@ class AssignVehicleDriverView(APIView):
 
         order.save()
 
-        return Response({"detail": "Order updated successfully"}, status=status.HTTP_200_OK)
+        return Response({"detail": "Pedido atulizado com sucesso"}, status=status.HTTP_200_OK)
 
 class AssingDriverView(APIView):
     def post(self, request, order_id, driver_id):
         order = get_object_or_404(Order, id=order_id)
         driver = get_object_or_404(Driver, id=driver_id)
+        orderStay = Order.objects.filter(id_driver=driver_id)
 
-        print(order.status) #retornou status 5
-
+        if orderStay.count() > 0:
+            for i in orderStay:
+                if [0, 1, 2, 3, 4, 5, 6, 7].count(i.status) > 0:
+                    return Response({"detail": "Motorista ocupado", "status": str(i.status), "order": str(i.id)}, status=status.HTTP_409_CONFLICT)
+                
         order.id_driver = driver
-
         order.save()
 
-        return Response({"detail": "Order updated successfully"}, status=status.HTTP_200_OK)
+        return Response({"detail": "Pedido atulizado com sucesso"}, status=status.HTTP_200_OK)
     
 class AssingVehicleView(APIView):
     def post(self, request, order_id, vehicle_id):
         order = get_object_or_404(Order, id=order_id)
         vehicle = get_object_or_404(Vehicle, id=vehicle_id)
+        orderStay = Order.objects.filter(id_vehicle=vehicle_id)
 
+        if orderStay.count() > 0:
+            for i in orderStay:
+                if [0, 1, 2, 3, 4, 5, 6, 7].count(i.status) > 0:
+                    return Response({"detail": "Veiculo já em uso", "status": str(i.status), "order": str(i.id)}, status=status.HTTP_409_CONFLICT)
+                
         order.id_vehicle = vehicle
 
         order.save()
 
-        return Response({"detail": "Order updated successfully"}, status=status.HTTP_200_OK)
+        return Response({"detail": "Pedido atulizado com sucesso"}, status=status.HTTP_200_OK)
